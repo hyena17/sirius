34c987bbfbc79b9046110af3bec1afe99a8ea6a4
Hyena <eli.delarosa.raymundo@gmail.com>
2017-04-17 19:39:38 -0700

    updating prod server to sa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    envName: 'prod',
-    apiHost: 'http://ec2-35-163-108-173.us-west-2.compute.amazonaws.com:3000'
+  production: true,
+  envName: 'prod',
+  apiHost: 'http://ec2-54-233-126-105.sa-east-1.compute.amazonaws.com:3000'
 };
